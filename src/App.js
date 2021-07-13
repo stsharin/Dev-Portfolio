294d5e9b49a45810536298a3cbb6a4e214b5792d
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-14 00:06:05 +0600

    started experience section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Education from './components/Education/Education';
+import Experience from './components/Experience/Experience';
 import Home from "./components/Home/Home";
 import Proficiency from './components/Proficiency/Proficiency';
 import Skills from "./components/Skills/Skills";
@@ -8,10 +9,11 @@ import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <div>
-      <Home/>
-      <Skills/>
-      <Proficiency/>
-      <Education/>
+      <Home />
+      <Skills />
+      <Proficiency />
+      <Education />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Experience.css'
+
+const Experience = () => {
+    return (
+        <div className="experience__section">
+            <div className="container">
+                <h1>Experiences</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
